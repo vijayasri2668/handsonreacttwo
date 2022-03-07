aedc0d79f6a4347de8a9b37f39e9038b6f1f52d3
Vijaya Sri <palivelavijayasri99@gmail.com>
2022-03-07 23:34:16 +0530

    handsonreacttwo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+        state={
+        name:"",
+        department:"",
+        rating:"",
+        user:[],
+    }
+    ButtonChange=(element)=>{
+        this.setState({
+        [element.target.name]:element.target.value
+    })
+    }
+
+    ButtonSubmit=(element)=>{
+        element.preventDefault()
+        console.log(this.state);
+
+        var temporaryArr=[]
+        console.log(temporaryArr);
+
+        var tempObj={
+        name: this.state.name,
+        deptt: this.state.department,
+        rating: this.state.rating
+        }
+        temporaryArr=this.state.user
+        temporaryArr.push(tempObj)
+        this.setState({user:temporaryArr})
+        this.setState({name:'',dept:'',rating:''})
+
+        console.log(this.state.user);
+    }
+
+  render() {
+    return (
+      <>    
+        <div className="Containerbody">
+            <div className="Header">Employe Feedback Form</div>
+            <form  onSubmit={this.ButtonSubmit} className='form_part'>
+                <label>Name:</label>
+                <input type="text" name="name" autoComplete='off'   className='inpt_forms' value={this.state.name} onChange={this.ButtonChange} /><br />
+                <label>Department:</label>
+                <input type="text" name="dept" autoComplete='off' className='inpt_forms'  value={this.state.department} onChange={this.ButtonChange} /><br />
+                <label>Rating:</label>
+                <input type="text" name="rating" autoComplete='off' className='inpt_forms'  value={this.state.rating} onChange={this.ButtonChange} /><br />
+                <button className='button' onClick={this.ButtonSubmit}>submit</button>
+            </form>
+            <div className="box">
+            {this.state.user.map((item,index)=>{
+                return(
+                    <div key={index} className='item' >
+                    Name :    {item.name} | Department :  {item.dept} | Rating:    {item.rating}
+                    </div>
+                    )
+               
+            })}
+            </div>
+          </div>
+
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
